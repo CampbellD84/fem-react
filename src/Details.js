57beca009e58009c01b9eeeb4fc33ded3ff149fc
Douglas Campbell <dacampbelltbone@gmail.com>
2019-09-12 01:53:23 -0400

    Context with hooks and class components

--- src/Details.js
+++ src/Details.js
@@ -2,15 +2,9 @@ import React, { Component } from "react";
 import pet from "@frontendmasters/pet";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
+import ThemeContext from "./ThemeContext";
 
 class Details extends Component {
-  // constructor(props) {
-  //   super(props);
-
-  //   this.state = {
-  //     loading: true
-  //   };
-  // }
   state = { loading: true };
   componentDidMount() {
     pet.animal(this.props.id).then(({ animal }) => {
@@ -39,7 +33,12 @@ class Details extends Component {
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} - ${breed} - ${location}`}</h2>
-          <button>Adopt {name}</button>
+          <ThemeContext.Consumer>
+            {([theme]) => (
+              <button style={{ backgroundColor: theme }}>Adopt {name}</button>
+            )}
+          </ThemeContext.Consumer>
+
           <p>{description}</p>
         </div>
       </div>
